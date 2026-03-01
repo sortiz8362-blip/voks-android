6a3107a34d946db7161a4bef1e6b5b95b14b8fda
sortiz8362-blip <sortiz8362@utm.edu.ec>
2026-02-28 21:52:38 -0500

    fase 2 terminada

---
+++ app/src/main/java/com/voks/social/core/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.voks.social.core.utils
+
+object Constants {
+    // Reemplaza "TU_PROJECT_ID_AQUI" por el ID real que te dio Appwrite
+    const val APPWRITE_PROJECT_ID = "69a39b2b002107ac9726"
+
+    // Si usas Appwrite Cloud, deja este link. Si usas local, pon tu IP ej: "http://192.168.1.100/v1"
+    const val APPWRITE_ENDPOINT = "https://sfo.cloud.appwrite.io/v1"
+
+    // Aquí iremos agregando los IDs de la Base de Datos y Colecciones en futuras fases
+    const val DATABASE_ID = ""
+    const val USERS_COLLECTION_ID = ""
+    const val POSTS_COLLECTION_ID = ""
+}
---
+++ app/src/main/java/com/voks/social/di/AppwriteModule.kt
@@ -0,0 +1,45 @@
+package com.voks.social.di
+
+import android.content.Context
+import com.voks.social.core.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.appwrite.Client
+import io.appwrite.services.Account
+import io.appwrite.services.Databases
+import io.appwrite.services.Storage
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppwriteModule {
+
+    @Provides
+    @Singleton
+    fun provideAppwriteClient(@ApplicationContext context: Context): Client {
+        return Client(context)
+            .setEndpoint(Constants.APPWRITE_ENDPOINT)
+            .setProject(Constants.APPWRITE_PROJECT_ID)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppwriteAccount(client: Client): Account {
+        return Account(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppwriteDatabases(client: Client): Databases {
+        return Databases(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppwriteStorage(client: Client): Storage {
+        return Storage(client)
+    }
+}
